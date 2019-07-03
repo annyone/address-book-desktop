0c7e548b9985d9368fcb9ec0200d1ab5c881596b
Анна Малинина <annyone.vbg@gmail.com>
2019-07-04 01:28:24 +0300

    8.1

---
+++ address-book-desktop/appmanager/GroupHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace address_book_desktop
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            OpenGroupsDialog();
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetItemCount", "#0", "");
+            for(int i = 0; i < int.Parse(count); i++)
+            {
+                string item = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetText", "#0|#"+i, "");
+                list.Add(new GroupData() { Name = item });
+
+            }
+            CloseGroupsDialog();
+            return list;
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            OpenGroupsDialog();
+            //aux.WinActive(GROUPWINTITLE);
+            //aux.WinWaitActive(GROUPWINTITLE);
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            aux.Send(newGroup.Name);
+            aux.Send("{ENTER}");
+            CloseGroupsDialog();
+        }
+
+        private void CloseGroupsDialog()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d54");
+        }
+
+        private void OpenGroupsDialog()
+        {
+            aux.ControlClick("", "", "WindowsForms10.BUTTON.app.0.2c908d5128");
+            aux.WinWait(GROUPWINTITLE);
+        }
+    }
+}
---
+++ address-book-desktop/tests/GroupCreationTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace address_book_desktop
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldList = app.Groups.GetList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "Group"
+            };
+            app.Groups.Add(newGroup);
+
+            List<GroupData> newList = app.Groups.GetList();
+            oldList.Add(newGroup);
+            oldList.Sort();
+            newList.Sort();
+
+            Assert.AreEqual(newList, oldList);
+        }
+    }
+}
